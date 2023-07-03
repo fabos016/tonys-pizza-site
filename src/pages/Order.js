f754fca83f334a1ce51ce584c4b22ccc178aad32
fabos016 <fabos016@uottawa.ca>
2023-07-02 23:26:46 -0400

    Fix #2 white page

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-import {Button} from "react-bootstrap";
-import {Link} from 'react-router-dom';
-
 import OrderContent from '../components/OrderContent';
 import NavbarOther from '../components/NavbarOther';
 
